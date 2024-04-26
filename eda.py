4bd3a13c8fd3d2718a073d72612945ed5fe91732
Gelieza K <gelieza.gk@gmail.com>
2024-04-26 13:02:19 +0200

    Use Dask and Hvplot for performance reasons

--- eda.py
+++ eda.py
@@ -1,10 +1,23 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from time import time
+import dask.dataframe as dd
+import hvplot
 
 
 def plot_features(df):
     """Just some plots of feature values, distribution, outliers,  aso"""
+    # Size of dataframe
+    print(df.shape)
+    print(df.info)
+    print(df.dtypes)
+
+    # Make boxplots for variables with range of numeric variables, see Assignment2 pdf, Table 1
+    df_num = df.select_dtypes(include=['float64'])
+    # Use hvplot for plotting because dataframe is too large!
+    #df_num.hvplot.box()
+    
     return
 
 
@@ -17,10 +30,24 @@ def plot_correlations(df):
     return
 
 if __name__ == '__main__':
-    # Import dataset
-    df = None
+    # Execute once: Import dataset
+    start = time()
+    df = dd.read_csv("training_set_VU_DM.csv")
+    end = time()
+    print(f"Loading csv takes {end-start}.") #22 secs with pandas, 0.02 s with dask
+    # Execute once: store to hdf5 for faster imports
+    #df.to_hdf("store.h5", key="table", append=True)
+
+    # Now always import from hdf5
+    #start= time()
+    #store = dd.read_hdf("store.h5", "table")
+    #end = time()
+    #print(f"Loading hdf5 takes {end - start}. ") # 13 seconds, much faster than csv
+    #df = dd.DataFrame(store)
     # Plot features
     plot_features(df)
     plot_missing_values(df)
     plot_correlations(df)
+    end2 = time()
+    print(f"Total runtime: {end2- start}") # 69 seconds with pandas
 
